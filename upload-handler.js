c4d25d359a6cc8e9cfca35d57ffbd37e78ac8ad0
codeaudit <ceperez@codeaudit.com>
2016-08-18 10:19:48 -0400

    add upload handler

---
+++ upload-handler.js
@@ -0,0 +1,12 @@
+// File handler
+
+exports.upload = function(req, res) {
+    console.log('upload');
+    var data = req.body;
+   
+    //res.redirect('/main.html');
+}
+
+exports.download = function(req, res) {
+    console.log('signup');
+}
